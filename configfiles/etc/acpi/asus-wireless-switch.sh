4064de55ac645e4d0da5d71bc5c9e8abec855d07
Andrei G <otzy007>
2012-05-19 13:10:45 +0300

    1st

---
+++ configfiles/etc/acpi/asus-wireless-switch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+WLANSTATUS=`cat /sys/class/ieee80211/phy0/rfkill*/state`
+
+test -z $WLANSTATUS && exit 1
+
+if [ $WLANSTATUS = 0 ]; then
+	echo 0 > /sys/devices/platform/asus_laptop/wlan
+	echo 1 > /sys/class/ieee80211/phy0/rfkill*/state
+elif [ $WLANSTATUS = 1 ]; then
+	echo 1 > /sys/devices/platform/asus_laptop/wlan
+	echo 0 > /sys/class/ieee80211/phy0/rfkill*/state
+fi
