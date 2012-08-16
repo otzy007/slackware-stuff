7aba86e367452f79e4cd73ef70cd9ec9d4b16fd6
Andrei G <otzy007>
2012-08-16 13:55:36 +0300

    modified:   asus-wireless-switch.sh: rfkill0 not *

--- etc/acpi/asus-wireless-switch.sh
+++ etc/acpi/asus-wireless-switch.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-
-# Use the Fn-F2 combination to disable/enable wireless on Asus K52
-
 WLANSTATUS=`cat /sys/class/ieee80211/phy0/rfkill*/state`
 
 test -z $WLANSTATUS && exit 1
 
 if [ $WLANSTATUS = 0 ]; then
 	echo 0 > /sys/devices/platform/asus_laptop/wlan
-	echo 1 > /sys/class/ieee80211/phy0/rfkill*/state
+	echo 1 > /sys/class/ieee80211/phy0/rfkill0/state
 elif [ $WLANSTATUS = 1 ]; then
 	echo 1 > /sys/devices/platform/asus_laptop/wlan
-	echo 0 > /sys/class/ieee80211/phy0/rfkill*/state
+	echo 0 > /sys/class/ieee80211/phy0/rfkill0/state
 fi
